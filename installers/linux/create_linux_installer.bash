6a67a0757d6cbf0c0012d77282390e8d898ec318
George Karagoulis <karagog@gmail.com>
2015-01-29 17:01:18 -0500

    Added the version information directly into the deploy script that creates the linux installer, so you can just run it without any arguments and without knowing the current version.

--- installers/linux/create_linux_installer.bash
+++ installers/linux/create_linux_installer.bash
@@ -1,14 +1,16 @@
 #! /bin/bash
 
+VERSION=3.0.1_alpha
+
 TOP_DIR=../..
 QT_DIR=/opt/Qt/5.4/gcc
-REPO_DIR=gryptonite_v$1
+REPO_DIR=gryptonite_v$VERSION
 
 mkdir $REPO_DIR
 
 # Copy gryptonite binaries
-cp $TOP_DIR/bin/*               $REPO_DIR
-cp $TOP_DIR/lib/*.so.3            $REPO_DIR
+cp $TOP_DIR/bin/*                               $REPO_DIR
+cp $TOP_DIR/lib/*.so.3                          $REPO_DIR
 
 # Copy GUtil libs
 cp $TOP_DIR/gutil/lib/libGUtil.so.0             $REPO_DIR
@@ -17,7 +19,7 @@ cp $TOP_DIR/gutil/lib/libGUtilCryptoPP.so.0     $REPO_DIR
 cp $TOP_DIR/gutil/lib/libGUtilAboutPlugin.so    $REPO_DIR
 
 # Copy crypto++
-cp /usr/lib/libcryptopp.so                $REPO_DIR
+cp /usr/lib/libcryptopp.so                      $REPO_DIR
 
 # Copy Qt libs
 cp $QT_DIR/lib/libQt5Core.so.5                  $REPO_DIR
@@ -25,7 +27,7 @@ cp $QT_DIR/lib/libQt5Gui.so.5                   $REPO_DIR
 cp $QT_DIR/lib/libQt5Widgets.so.5               $REPO_DIR
 cp $QT_DIR/lib/libQt5Sql.so.5                   $REPO_DIR
 cp $QT_DIR/lib/libQt5Xml.so.5                   $REPO_DIR
-cp $QT_DIR/lib/libQt5DBus.so.5                   $REPO_DIR
+cp $QT_DIR/lib/libQt5DBus.so.5                  $REPO_DIR
 cp $QT_DIR/lib/libicui18n.so.53                 $REPO_DIR
 cp $QT_DIR/lib/libicuuc.so.53                   $REPO_DIR
 cp $QT_DIR/lib/libicudata.so.53                 $REPO_DIR
@@ -37,12 +39,12 @@ cp $QT_DIR/plugins/sqldrivers/libqsqlite.so     $REPO_DIR/sqldrivers
 cp $QT_DIR/plugins/platforms/*                  $REPO_DIR/platforms
 
 # Copy install scripts
-cp INSTALL                              $REPO_DIR
-cp $TOP_DIR/LICENSE                     $REPO_DIR
-cp $TOP_DIR/NOTICE                      $REPO_DIR
-cp $TOP_DIR/installers/LICENSE_CRYPTOPP $REPO_DIR
-cp $TOP_DIR/installers/LICENSE_QT       $REPO_DIR
-cp install.bash                         $REPO_DIR
+cp INSTALL                                      $REPO_DIR
+cp $TOP_DIR/LICENSE                             $REPO_DIR
+cp $TOP_DIR/NOTICE                              $REPO_DIR
+cp $TOP_DIR/installers/LICENSE_CRYPTOPP         $REPO_DIR
+cp $TOP_DIR/installers/LICENSE_QT               $REPO_DIR
+cp install.bash                                 $REPO_DIR
 chmod +x $REPO_DIR/install.bash
 
 archive=$REPO_DIR.tar.gz

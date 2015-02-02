ff5884e1c1a4a136c5a986da468edaa1d74c04c8
George Karagoulis <karagog@gmail.com>
2015-02-02 10:35:50 -0500

    Improved the linux install script, trying to make it more idiot-proof. Now you can just update the first variable to change the root of the installation

--- installers/linux/install.bash
+++ installers/linux/install.bash
@@ -3,15 +3,26 @@
 # You may update these locations to install wherever you want. BUT
 #  BE CAREFUL if you update this because the uninstaller will
 #  remove the entire directory!
-INSTALL_DIR_LIBS="/usr/local/lib/gryptonite"
+INSTALL_BASE_DIR="/usr/local"
+INSTALL_DIR_LIBS="$INSTALL_BASE_DIR/lib/gryptonite"
+INSTALL_DIR_BIN="$INSTALL_BASE_DIR/bin"
 
 # You will call this startup script to launch gryptonite. It exports
 #  the proper LD_LIBRARY_PATH and calls the executable in the lib directory
-START_LOC="/usr/local/bin/gryptonite"
+START_LOC="$INSTALL_DIR_BIN/gryptonite"
 
 # This is a fancy expression that gets the install script's current directory
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SCRIPT_DIR
+
+# Create the installation directories
+if [ ! -d "$INSTALL_DIR_LIBS" ]; then
+    mkdir -p "$INSTALL_DIR_LIBS"
+fi
+
+if [ ! -d "$INSTALL_DIR_BIN" ]; then
+    mkdir -p "$INSTALL_DIR_BIN"
+fi
 
 # Create an uninstall script
 echo "#! /bin/bash"                 > uninstall.bash
@@ -26,20 +37,14 @@ echo "export LD_LIBRARY_PATH=$INSTALL_DIR_LIBS"      >> $START_LOC
 echo "$INSTALL_DIR_LIBS/gryptonite"                 >> $START_LOC
 chmod +x $START_LOC
 
-# Create the installation directory
-if [ ! -d "$INSTALL_DIR_LIBS" ]; then
-    mkdir "$INSTALL_DIR_LIBS";
-fi;
-
-
 if [ -d "$INSTALL_DIR_LIBS" ]; then
-    cp -r * "$INSTALL_DIR_LIBS";
-    chmod 755 -R "$INSTALL_DIR_LIBS";
-fi;
+    cp -r * "$INSTALL_DIR_LIBS"
+    chmod 755 -R "$INSTALL_DIR_LIBS"
+fi
 
 
 # Show confirmation message
 if [ -f "$START_LOC" ]; then
     echo "Gryptonite libraries installed to $INSTALL_DIR_LIBS"
     echo "Start application with:  $START_LOC"
-fi;
+fi

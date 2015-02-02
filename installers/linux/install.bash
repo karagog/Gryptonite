5bb7e569ec9360b225deb99e7dad744073011544
George Karagoulis <karagog@gmail.com>
2015-02-02 13:27:30 -0500

    Added grypto_transforms to the default install. Now it will be right alongside the other startup script. Without this new startup script, you wouldn't be able to manually start it without setting LD_LIBRARY_PATH, so that's why I wanted to make this as easy as possible.

--- installers/linux/install.bash
+++ installers/linux/install.bash
@@ -10,6 +10,7 @@ INSTALL_DIR_BIN="$INSTALL_BASE_DIR/bin"
 # You will call this startup script to launch gryptonite. It exports
 #  the proper LD_LIBRARY_PATH and calls the executable in the lib directory
 START_LOC="$INSTALL_DIR_BIN/gryptonite"
+START_LOC_TRANSFORMS="$INSTALL_DIR_BIN/grypto_transforms"
 
 # This is a fancy expression that gets the install script's current directory
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -27,15 +28,20 @@ fi
 # Create an uninstall script
 echo "#! /bin/bash"                 > uninstall.bash
 echo "rm $START_LOC"                >> uninstall.bash
+echo "rm $START_LOC_TRANSFORMS"     >> uninstall.bash
 echo "rm -rf $INSTALL_DIR_LIBS"     >> uninstall.bash
 chmod +x uninstall.bash
 
 # Create a startup script
 echo "#! /bin/bash"                                 > $START_LOC
-#echo "export QT_PLUGIN_PATH=$INSTALL_DIR_LIBS"      >> $START_LOC
 echo "export LD_LIBRARY_PATH=$INSTALL_DIR_LIBS"      >> $START_LOC
+cp $START_LOC $START_LOC_TRANSFORMS
+
 echo "$INSTALL_DIR_LIBS/gryptonite"                 >> $START_LOC
+echo "$INSTALL_DIR_LIBS/grypto_transforms"          >> $START_LOC_TRANSFORMS
+
 chmod +x $START_LOC
+chmod +x $START_LOC_TRANSFORMS
 
 if [ -d "$INSTALL_DIR_LIBS" ]; then
     cp -r * "$INSTALL_DIR_LIBS"

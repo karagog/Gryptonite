b1cf95effd2b095cf90dda1372b7c03d1d71dced
George Karagoulis <karagog@gmail.com>
2015-02-11 12:12:20 -0500

    Enable the export button when the file exists

--- src/presentation/controls/entryview.cpp
+++ src/presentation/controls/entryview.cpp
@@ -64,8 +64,10 @@ void EntryView::SetEntry(const Entry &e)
     else
     {
         ui->lbl_fileName->setText(e.GetFileName());
-        if(m_dbModel->FileExists(e.GetFileId()))
+        if(m_dbModel->FileExists(e.GetFileId())){
             ui->lbl_fileStatus->setText(tr("(Uploaded)"));
+            ui->btn_exportFile->setEnabled(true);
+        }
         else{
             ui->lbl_fileStatus->setText(tr("(Missing)"));
             ui->btn_exportFile->setEnabled(false);
